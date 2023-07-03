469ce30a0ee229b4d301bd9abc1f2cd4a1a720cd
Fraditya Aryatama <aryatamafraditya99@gmail.com>
2023-07-03 19:48:04 +0700

    update code

--- Fraditya/cypress/e2e/add_to_cart.cy.js
+++ Fraditya/cypress/e2e/add_to_cart.cy.js
@@ -6,6 +6,7 @@ describe('login until adding item to cart', () => {
     
     // Enter username and password
     cy.wait(1000)
+    cy.get('#logInModalLabel.modal-title').should('be.visible')
     cy.get('input[id="loginusername"]').type('fraditya')
     cy.get('input[id="loginpassword"]').type('fraditya')
 
@@ -14,12 +15,28 @@ describe('login until adding item to cart', () => {
     cy.wait(1000)
     cy.get('#logout2').should('be.visible')
     cy.get('#nameofuser').should('be.visible')
+    cy.wait(1000)
     
 
-
-
     // Adding item to cart section
-    // cy.xpath("//a[.='Samsung galaxy s6']").click()
+    // item 1
+    cy.xpath('//a[.="Samsung galaxy s6"]').first().click() 
+    cy.wait(1000)
+    cy.get('.name').should('be.visible') 
+    cy.xpath('//a[contains(text(),"Add to cart")]').click()
+    cy.wait(1000)
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('Product added.')
+    })
+    // make sure to cart page
+    cy.get('#cartur').click()
+    cy.wait(1000)
+    cy.xpath('//a[contains(text(),"Home")]').click()
+
+
+    // item 2
+    
+
 
 
   })
