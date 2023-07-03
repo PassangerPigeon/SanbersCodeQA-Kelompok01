f2931a5e2e660858bf26f7d5954169b7363d7e57
Fraditya Aryatama <aryatamafraditya99@gmail.com>
2023-07-03 17:55:09 +0700

    update code

--- Fraditya/cypress/e2e/add_to_cart.cy.js
+++ Fraditya/cypress/e2e/add_to_cart.cy.js
@@ -1,15 +1,26 @@
-describe('login_success', () => {
-  it('should log in successfully', () => {
+describe('login until adding item to cart', () => {
+  it('should log in and succesfully adding item to cart', () => {
     cy.visit('https://demoblaze.com/') // Replace with your login page URL
     
     cy.get('.nav-link[id="login2"]').click()
     
     // Enter username and password
+    cy.wait(1000)
     cy.get('input[id="loginusername"]').type('fraditya')
     cy.get('input[id="loginpassword"]').type('fraditya')
 
     // Click on the login button
     cy.xpath('//button[.="Log in"]').click()
+    cy.wait(1000)
+    cy.get('#logout2').should('be.visible')
+    cy.get('#nameofuser').should('be.visible')
+    
+
+
+
+    // Adding item to cart section
+    // cy.xpath("//a[.='Samsung galaxy s6']").click()
+
 
   })
 })
