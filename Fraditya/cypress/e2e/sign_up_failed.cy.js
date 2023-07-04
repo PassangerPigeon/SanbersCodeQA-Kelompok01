a9a9cba307b190d3cc357b3ccaae5366e32135a8
Fraditya Aryatama <aryatamafraditya99@gmail.com>
2023-07-04 16:04:48 +0700

    update

--- Fraditya/cypress/e2e/sign_up_failed.cy.js
+++ Fraditya/cypress/e2e/sign_up_failed.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('sign up', () => {
+  it('should cannot sign up', () => {
+    cy.visit('https://demoblaze.com/') // Replace with your login page URL
+
+    cy.get('#signin2').click()
+    cy.wait(1000)
+    cy.get('input[id="sign-username"]').type('fraditya')
+    cy.get('input[id="sign-password"]').type('fraditya')
+
+    cy.xpath('//button[.="Sign up"]').click()
+    cy.on('window:alert', (alertText) => {
+      expect(alertText).to.equal('This user already exist.')
+    })
+    cy.get('#login2').should('be.visible')
+
+
+    
   })
 })
